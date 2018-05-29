e30b8cb0db6a054729193c72e13a574492156a41
h4nds0m3j4ck <estrella.terenzio@gmail.com>
2018-05-29 17:11:37 -0400

    created stylesheet and added some variables to javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+// Random Letter Variables //
+var letters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var randomLetter = getRandomLetter(26);
+
+// User's Guess//
+var numGuess = 7;
+var guess = prompt("What's your guess?");
+var userAnswer = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var guessCount = 0;
+var correctGuess = false;
+
+//Alert 
+alert("What's your guess?");
+
+// Loop //
+for (i=0; i < 7; i++ );{
+
+}

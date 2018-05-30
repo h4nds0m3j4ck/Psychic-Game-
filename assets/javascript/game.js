c31ccb6490d172be36ce1e8395fae205e5da685b
h4nds0m3j4ck <estrella.terenzio@gmail.com>
2018-05-29 22:11:56 -0400

    added style and reset.css pages, as well as tweaked game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,24 @@
-// Random Letter Variables //
+//Prompt the user to pick one of the variable letters in the array 
 var letters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var randomLetter = getRandomLetter(26);
+// Generate a random letter and store it as a variable 
+var randomLetter = Math.floor(Math.random() * userLetter) +1;
+var userLetter = 26;
 
-// User's Guess//
+// Prompt user to guess a letter A-Z//
 var numGuess = 7;
-var guess = prompt("What's your guess?");
+var guess = prompt("Pick a letter between A -Z. What's your guess?");
 var userAnswer = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var guessCount = 0;
 var correctGuess = false;
 
+// Check to see if the guess was correct //
+
+// If the letter selected is wrong, prompt the user to try again and choose another letter. Repeat until the correct answer is chosen or user exhausted the attempts. //
+
+//If user chose the correct letter, display confirmation text // 
+
 //Alert 
-alert("What's your guess?");
+//alert("What's your guess?");//
 
 // Loop //
 for (i=0; i < 7; i++ );{

ac0fad76bfceba6269375fb0ee07aa28123722ff
h4nds0m3j4ck <estrella.terenzio@gmail.com>
2018-05-30 13:40:44 -0400

    removed game.js, tweaked style.css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //Prompt the user to pick one of the variable letters in the array 
-var letters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
 // Wins, losses, guesses so far counter
@@ -12,22 +12,63 @@ var guessLeft = 7; // 7 tries to guess the letter
 document.onkeyup = function(event){
 
     // Determines what key was pressed 
-    var userGuess = event.key;
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     // Generate a random letter and store it as a variable 
     var compGuess = letters[Math.floor(Math.random() * letters.length)];
     guessSoFar.push(userGuess); // Place user guess into array
     if (userGuess == randomLetter){
         wins++;
         alert("Hmm... you actually got it? You're a psychic! You win!");
-        
+        guessLeft = 7; // Resets guess counter
+        guessSoFar.length = 0; // Resets letters chosen in array
     }
-}
+    else if (guessLeft == 0){
+        losses++;
+        alert('Aww, you didn\'t get it... too bad! Wanna try again?!');
+        guessLeft = 7;
+        guessSoFar.length = 0;
+    }
+    else if (userGuess !== compGuess){
+        guessesLeft--; //decrementing the guesses left
+    }  
+
+
+    // Showing data via HTML
+    var html = "<h1>The Psychic Game</h1>" + 
+        "<p>Can you guess what letter I'm thinking of?! ;)</p>" +
+        "<p> Wins: " + 
+        wins + 
+        "</p>" +
+        "<p>Losses: " + 
+        losses + 
+        "</p>" +
+        "<p>Guesses Left: " + 
+        guessLeft + 
+        "</p>" +
+        "<p>Your Guesses so far: " +
+        guessSoFar +
+        "</p>"
+        ;
+        // Placing the html into the game ID
+        document.querySelector('#game').innerHTML = html;
+}  
+
+
+
+
+
+
+
+
+
+
+
     // Prompt user to guess a letter A-Z//
-    var numGuess = 7;
-    var guess = prompt("Pick a letter between A -Z. What's your guess?");
+    //var numGuess = 7;
+   // var guess = prompt("Pick a letter between A -Z. What's your guess?");
     
-    var guessCount = 0;
-    var correctGuess = false;
+    //var guessCount = 0;
+    //var correctGuess = false;
 
 // Check to see if the guess was correct //
 

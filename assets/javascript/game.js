35d0b2fc64b6825ddf45cbfbccb438467c1ca799
h4nds0m3j4ck <estrella.terenzio@gmail.com>
2018-05-30 12:55:09 -0400

    added game2.js as a test

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,33 @@
 //Prompt the user to pick one of the variable letters in the array 
 var letters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-// Generate a random letter and store it as a variable 
-var randomLetter = Math.floor(Math.random() * userLetter) +1;
-var userLetter = 26;
 
-// Prompt user to guess a letter A-Z//
-var numGuess = 7;
-var guess = prompt("Pick a letter between A -Z. What's your guess?");
-var userAnswer = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var guessCount = 0;
-var correctGuess = false;
+
+// Wins, losses, guesses so far counter
+var wins = 0;
+var losses = 0;
+var guessSoFar = []; // Array for user inputs
+var guessLeft = 7; // 7 tries to guess the letter
+
+// This function is used when the user inputs a character 
+document.onkeyup = function(event){
+
+    // Determines what key was pressed 
+    var userGuess = event.key;
+    // Generate a random letter and store it as a variable 
+    var compGuess = letters[Math.floor(Math.random() * letters.length)];
+    guessSoFar.push(userGuess); // Place user guess into array
+    if (userGuess == randomLetter){
+        wins++;
+        alert("Hmm... you actually got it? You're a psychic! You win!");
+        
+    }
+}
+    // Prompt user to guess a letter A-Z//
+    var numGuess = 7;
+    var guess = prompt("Pick a letter between A -Z. What's your guess?");
+    
+    var guessCount = 0;
+    var correctGuess = false;
 
 // Check to see if the guess was correct //
 
@@ -17,10 +35,11 @@ var correctGuess = false;
 
 //If user chose the correct letter, display confirmation text // 
 
-//Alert 
-//alert("What's your guess?");//
-
 // Loop //
-for (i=0; i < 7; i++ );{
+//for (i = 0; i < letters.length; i++ ){
+    //console.log("Letters " + letters[i]);
+//}
 
-}
+// HTML //
+//var html = 
+  //  "<p>You chose " + userGuess + "<"
